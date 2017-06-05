a50e3539f061441138a686b019fb799a7748ffa6
Anwesh43 <anweshthecool0@gmail.com>
2017-06-05 11:38:36 +0530

    testing the promise function

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 const ShapedImageCreator = require('./lib')
-ShapedImageCreator.createShapeLike('stp.jpg','circle')
-ShapedImageCreator.createShapeLike('stp.jpg','triangle','test_triangle.png')
+ShapedImageCreator.createShapeLike('stp.jpg','circle').then(()=>{
+    console.log("circular image is saved successfully")
+}).catch((err)=>{
+    console.log(err)
+})
+ShapedImageCreator.createShapeLike('stp.jpg','triangle','test_triangle.png').then(()=>{
+    console.log("triangular image is saved successfully")
+}).catch((err)=>{
+    console.log(err)
+})
